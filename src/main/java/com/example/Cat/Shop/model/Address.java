d2bfbd46825f92f26ddcf165cd660408440a362e
Nguyen Chung Van <nguyenchungvan.t1.15cla@gmail.com>
2024-02-21 16:19:19 +0700

    Initial commit 3

--- src/main/java/com/example/Cat/Shop/model/Address.java
+++ src/main/java/com/example/Cat/Shop/model/Address.java
@@ -1,2 +1,27 @@
-package com.example.Cat.Shop.model;public class Address {
+package com.example.Cat.Shop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "ID")
+    private User user;
+
+    private String province;
+    private String district;
+    private String ward;
+    private String phoneNumber;
+    private String streetAndDepartment;
+    private String type;
+    private Boolean addressDefault;
+    private String nameReceiver;
+
+
 }
